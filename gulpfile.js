4072edb5a79acf0ae5f4b1429c39e4e7c70d6f09
Halil İbrahim Çakıroğlu <ibrahimchll@hotmail.com>
2019-09-24 12:15:08 +0300

    Gulp configuration file is generated.

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var { watch, src, dest } = require("gulp");
+var sass = require("gulp-sass");
+var autoprefixer = require("gulp-autoprefixer");
+var sourcemaps = require("gulp-sourcemaps");
+
+function compileSass() {
+  return (
+    src(["./**/*.scss", "!node_modules/**/*.scss"], { base: "." })
+      .pipe(sourcemaps.init())
+      // gulp-sass kullanarak Sass dosyasını CSS'e çeviriyor. "nested", "compact", "expanded", "compressed" değerleri kullanılabilir.
+      .pipe(sass({ outputStyle: "expanded" }))
+      .pipe(sourcemaps.write())
+      .pipe(
+        autoprefixer({
+          Browserslist: ["last 1 version", "iOS 6"],
+          cascade: false
+        })
+      )
+      .pipe(dest("./"))
+  );
+}
+
+exports.default = function() {
+  watch("./**/*.scss", compileSass);
+};
