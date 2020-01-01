57455aea96ea338c62967595f5903fe6d5d04165
Halil İbrahim Çakıroğlu <ibrahimchll@hotmail.com>
2020-01-01 22:06:24 +0300

    Chokidar watcher is added.

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var { watch, src, dest } = require("gulp");
 var sassify = require("gulp-sass");
 var autoprefixer = require("gulp-autoprefixer");
 var sourcemaps = require("gulp-sourcemaps");
+var watcher = require("gulp-chokidar");
 
 function sass() {
   return (
@@ -24,7 +25,28 @@ function sass() {
   );
 }
 
+function sass_only_modified() {
+  return (
+    src(".")
+      .pipe(sourcemaps.init())
+      .pipe(sassify({ outputStyle: "expanded" }))
+      .on("error", function swallowError(error) {
+        console.log(error.toString());
+        this.emit("end");
+      })
+      .pipe(sourcemaps.write())
+      .pipe(
+        autoprefixer({
+          overrideBrowserslist: ["last 4 version"],
+          cascade: false
+        })
+      )
+      .pipe(dest("./"))
+  );
+}
+
+
 exports.sass = sass;
-exports.watch = function() {
-  watch("./**/*.scss", sass);
+exports.watch = function () {
+  watcher("./**/*.scss", sass);
 };
